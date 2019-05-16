00b0040e7e323a961ca4dd2ba72c6063a27c6fff
sunt <1070800554@qq.com>
2019-05-16 22:18:13 +0800

    首次上传

---
+++ Datestruct1/Datestruct1/DivideAndConquer.h
@@ -0,0 +1,4 @@
+#pragma once
+int Max3(int A, int B, int C);
+int DivideAndConquer(int List[], int left, int right);
+int MaxsubseqSum3(int List[], int N);
---
+++ Datestruct1/Datestruct1/YangMain.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"DivideAndConquer.h"
+main() {
+	int a = 7, b = 5, c = 8,Max;
+	Max= Max3(a, b, c);
+	printf("%d", Max);
+	 system("pause" );
+	 int List[] = {3,6,2,7,4,1};
+	 int left,  right,Sum;
+	  Sum=DivideAndConquer(List[left], left=0, right=5);
+	  printf("%d", Sum);
+	  system("pause");
+	// MaxsubseqSum3( List[left], 6);
+	// system("pause");
+}
